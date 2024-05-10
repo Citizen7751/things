38d2ea5c83803d0c12578b71737011930b3b9815
Citizen <166248493+Citizen7751@users.noreply.github.com>
2024-05-10 19:44:33 +0200

    Add files via upload

---
+++ BogoSort.cpp
@@ -0,0 +1,82 @@
+/*----------------BogoSort------------------
+
+Compile with GCC:
+
+	Windows:
+	g++ -o bogosort.exe bogosort.cpp
+
+------------------------------------------*/
+
+#include <iostream>
+#include <windows.h>	//Sleep	
+#include <time.h>		//random -> time
+#include <stdlib.h>		//random
+#include <vector>
+
+typedef std::vector<double> dvec;
+
+
+namespace BogoSort {
+
+	void print_elements(dvec& v) {
+		for (int i = 0; i < v.size(); i++)
+			std::cout << v[i] << ' ';
+		std::cout << '\n';
+	}
+
+	int is_sorted(dvec& v) {
+		for (int i = 1; i<v.size(); i++) 
+			if (v[i-1] > v[i]) return 0;
+		return 1;
+	}
+
+	void mix_elements(dvec& v) {
+
+		int i1, i2;
+		srand(time(NULL));
+
+		for (int i = 0; i < v.size(); i++) {
+			
+			i1 = rand()%v.size();	//index of the first chosen element
+			i2 = rand()%v.size();	//index of the second chosen element
+			
+			std::swap(v[i1], v[i2]);
+		}
+	}
+
+//----------------------------------------------------------------------------
+// You can choose to just sort, or sort verbosely
+
+	void Sort_Verbose(dvec& v, int& i) {
+		while(!is_sorted(v)) {
+			mix_elements(v);
+			i++;
+			print_elements(v);
+			Sleep(300);
+		}
+	}
+
+	void Sort_Quiet(dvec& v, int& i) {
+		while(!is_sorted(v)) {
+			mix_elements(v);
+			i++;
+		}
+	}
+}
+
+
+int main(void) {
+	
+	dvec nums = {
+		1, 8, 4, 3
+	};
+	
+	int iterations = 0;
+
+	BogoSort::Sort_Verbose(nums, iterations);
+	//BogoSort::Sort_Quiet(nums, iterations);
+	
+	std::cout << "\nDone in " << iterations << " iteration(s).\n ";
+	std::cin.get();
+	return 0;
+}
---
+++ readytouse/bgs.hpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+typedef std::vector<double> dvec;
+
+namespace BogoSort {
+	int is_sorted(dvec& v);
+	void mix(dvec& v);
+	void Sort(dvec& v);
+}
